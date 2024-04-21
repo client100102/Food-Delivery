56b1bce55edd17c80f743b149b487183921ea107
client100102 <Duybao23032003@gmail.com>
2024-04-21 20:31:01 +0700

    second

---
+++ Food-Delivery/src/main/java/com/springboot/FoodDelivery/api/NewAPI.java
@@ -0,0 +1,13 @@
+package com.springboot.FoodDelivery.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewAPI {
+
+	@GetMapping("/test")
+	public String testAPI() {
+		return "success";
+	}
+}
